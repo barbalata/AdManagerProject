b2e0cfed4412e15c6833cd68ac3863dfdc9dd8a0
Cătălin-Răzvan Barbălată <barbalata.razvan@gmail.com>
2018-01-05 16:54:16 +0200

    Edit SQLConf Class

--- src/utils/SqlConf.java
+++ src/utils/SqlConf.java
@@ -1,13 +1,23 @@
 package utils;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class SqlConf {
+    //Declare JDBC Driver
+    private static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+
+    //Connection
+    private static Connection connection = null;
+
+    //Connection String
+    //String connStr = "jdbc:sqlserver://ServerName:1433;databaseName=DatabaseNameValue;user=UserNameValue;password=PasswordValue;IntegratedSecurity=IntegratedSecurityValue";
+    private static String connectionString = null;
 
     private static String SetConnectionURL() {
         utils.ConfigurationFile configurationFile = new ConfigurationFile();
-        return "jdbc:sqlserver://" +
+        connectionString = "jdbc:sqlserver://" +
                 configurationFile.getServer() +
                 ":1433;" +
                 "databaseName=" +
@@ -18,19 +28,22 @@ public class SqlConf {
                 configurationFile.getPassword() +
                 ";integratedSecurity=" +
                 configurationFile.getIntegratedSecurity();
+        return connectionString;
     }
 
     public static String SetConnectionURL(String ServerName, Boolean InterogatedSecurity) {
-        return "jdbc:sqlserver://" + ServerName + ":1433;"
+        connectionString = "jdbc:sqlserver://" + ServerName + ":1433;"
                 + "integratedSecurity=" + InterogatedSecurity;
+        return connectionString;
     }
 
     public static String SetConnectionURL(String ServerName, String DatabaseName, Boolean InterogatedSecurity, String User, String Password) {
-        return "jdbc:sqlserver://" + ServerName + ":1433;"
+        connectionString = "jdbc:sqlserver://" + ServerName + ":1433;"
                 + "databaseName=" + DatabaseName + ";"
                 + "user=" + User + ";"
                 + "password=" + Password + ";" + ""
                 + "integratedSecurity=" + InterogatedSecurity;
+        return connectionString;
     }
 
     public static Boolean TestConnection() {
@@ -43,7 +56,7 @@ public class SqlConf {
     public static Boolean TestConnection(String ConnectionString) {
         try {
             //STEP 2: Register JDBC driver
-            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            Class.forName(JDBC_DRIVER);
 
             //STEP 3: Open a connection
             System.out.println("Connecting to database...");
@@ -54,5 +67,39 @@ public class SqlConf {
             return false;
         }
         return true;
+    }
+
+    //Connect to DB
+    public static void dbConnect() throws SQLException, ClassNotFoundException {
+        //Setting Oracle JDBC Driver
+        try {
+            Class.forName(JDBC_DRIVER);
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your Oracle JDBC Driver?");
+            e.printStackTrace();
+            throw e;
+        }
+
+        System.out.println("Oracle JDBC Driver Registered!");
+
+        //Establish the Oracle Connection using Connection String
+        try {
+            connection = DriverManager.getConnection(connectionString);
+        } catch (SQLException e) {
+            System.out.println("Connection Failed! Check output console" + e);
+            e.printStackTrace();
+            throw e;
+        }
+    }
+
+    //Close Connection
+    public static void dbDisconnect() throws SQLException {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (Exception e) {
+            throw e;
+        }
     }
 }
